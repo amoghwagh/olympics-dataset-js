3a5c9bda0bf591ad6ffd2b786513d8feac8f2b85
amoghwagh <amoghwagh@gmail.com>
2019-09-01 21:21:20 +0530

    Top 10 Countries Function Created

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 let fs = require('fs')
 let csvToJsonFunc = require('./src/csvtojson.js');
 let Olympics = require('./src/olympics.js')
+
+csvToJsonFunc('./Dataset/athlete_events.csv').then((athlete_events) => {
+        csvToJsonFunc('./Dataset/noc_regions.csv').then((noc_regions) => {
+                console.log(Olympics.topTenCountries(athlete_events,noc_regions))
+        })  
+})
--- src/olympics.js
+++ src/olympics.js
@@ -1,8 +1,68 @@
 /*
-1) Number of times olympics hosted per city over the years - Piechart
+1) Number of times olympics hosted per city over the NOCs - Piechart
 2) Top 10 countries who have won most medals after 2000 - stacked column - split gold/silver/bronze
 3) M/F participation by decade - column chart
 4) Per season average age of athletes who participated in Boxing Men’s Heavyweight - Line
 5) Find out all medal winners from India per season - Table
 */
 
+function topTenCountries (eventsJson,nocJson) {
+    let noc = eventsJson.reduce((acc,event) => {
+        if( event["Year"] > 2000 && event["Medal"] != 'NA' ) {
+            if(acc[event["NOC"]]) {
+                ( acc[event["NOC"]][event["Medal"]] ) ? acc[event["NOC"]][event["Medal"]]++ : acc[event["NOC"]][event["Medal"]] = 1;
+                acc[event["NOC"]]["totalMedals"]++
+            } 
+                
+            else
+                (acc[event["NOC"]]) = { [event["Medal"]] : 1 ,
+                                        totalMedals : 1  ,  
+                                        key : event["NOC"]};
+        }
+
+        return acc; 
+    },{})
+
+    let topTenNoc = Object.values(noc).sort( (a,b) =>b["totalMedals"] - a["totalMedals"]).slice(0,10);
+    let countryToRegionMap = new Map();
+
+    for (let nocMapping of nocJson ) {
+        countryToRegionMap.set(nocMapping["NOC"],nocMapping["region"])
+    }
+    let topTenCountries = topTenNoc.reduce((acc,noc)=> {
+        acc[countryToRegionMap.get(noc["key"])] = {
+            "Gold" : ( noc['Gold'] ) ? noc['Gold'] : 0,
+            "Bronze" : ( noc['Bronze'] ) ? noc['Bronze'] : 0,
+            "Silver" : ( noc['Silver'] ) ? noc['Silver'] : 0
+        }
+        return acc;
+    },{})
+    return topTenCountries
+
+//     let categories = Object.keys(topTenCountries)
+//     let medalsArray = new Array(3).fill(0);
+//     for(let medals in medalsArray) {
+//         medalsArray[medals] = new Array();
+//     }
+
+//     for(let country in topTenCountries) {
+//         medalsArray[0].push(topTenCountries[country]["Gold"])
+//         medalsArray[1].push(topTenCountries[country]["Silver"])
+//         medalsArray[2].push(topTenCountries[country]["Bronze"])
+//     }
+
+//     let series = [{
+//         name: 'Gold',
+//         data: medalsArray[0]
+//     }, {
+//         name: 'Silver',
+//         data: medalsArray[1]
+//     }, {
+//         name: 'Bronze',
+//         data: medalsArray[2]
+//     }]
+}
+
+module.exports = {
+    topTenCountries: topTenCountries
+} 
