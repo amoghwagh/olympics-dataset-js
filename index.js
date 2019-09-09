0ae215faa461562bd525c1e3a7f4e403bf7f1936
amoghwagh <amoghwagh@gmail.com>
2019-09-09 14:05:51 +0530

    Fixed the undefined in medals Table

--- index.js
+++ index.js
@@ -8,13 +8,13 @@
   csv('./Dataset/athlete_events.csv').then((athlete_events) => {
     csv('./Dataset/noc_regions.csv').then((noc_regions) => {
       let jsonData = {} //Empty Json object is assigned and later populated with outputs from each function
-      
+
       jsonData["numberOfCities"] = Olympics.numberOfCities(athlete_events)
       jsonData["topCountries"] = Olympics.topCountries(athlete_events, noc_regions, 10)
       jsonData["numberOfParticipants"] = Olympics.numberOfParticipants(athlete_events)
       jsonData["averageAgeBoxing"] = Olympics.averageAgeBoxing(athlete_events)
       jsonData["medalWinners"] = Olympics.medalWinners(athlete_events)
 
-      fs.writeFileSync('./public/data.json', JSON.stringify(jsonData, null, 2))//Javascript Object is written to JSON
+      fs.writeFileSync('./public/data.json', JSON.stringify(jsonData, null, 2)) //Javascript Object is written to JSON
     })
   })
--- public/medalsTable.js
+++ public/medalsTable.js
@@ -1,14 +1,16 @@
 function createTable(medalWinnersJson) { // A table is created and rows are dynamically alloted based on the number of years present
   const table = document.getElementById('medalTable');
   const yearKeys = Object.keys(medalWinnersJson);
+  console.log(yearKeys)
   let rowCount = 0;
   for (const year of Object.keys(medalWinnersJson)) {
-    const row = table.insertRow(rowCount + 1); // New Row is created
+    const row = table.insertRow(rowCount+1); // New Row is created
     const yearCell = row.insertCell(0); // New cell is created at position 0
     const nameCell = row.insertCell(1); // New cell is created at position 1
 
-    yearCell.innerHTML = yearKeys[rowCount += 1]; //  Year is added at position 0
-    nameCell.innerHTML = medalWinnersJson[year]; //  Winner's name is added at position 1
+    yearCell.innerHTML = yearKeys[rowCount]; //  Year is added at position 0 of the row
+    nameCell.innerHTML = medalWinnersJson[year]; //  Winner's name is added at position 1 of the row
+    rowCount += 1;
   }
 }
 
